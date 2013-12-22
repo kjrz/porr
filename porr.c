b5711ce41ca8bc46a862ac3628661d88b30db982
kjrz <kuba.jarz@gmail.com>
2013-12-22 18:52:31 +0100

    removed unneccesary includes, changed graph type to float, cost changed to float, added auction-sse run

--- porr.c
+++ porr.c
@@ -2,8 +2,21 @@
 // JAKUB JARZYNSKI & LUKASZ RUTA
 
 #include "porr.h"
+
 #include <stdio.h>
 #include <time.h>
+#include <malloc.h>
+
+int *init_path() {
+  int i, *path;
+
+  path = (int *) malloc(GRAPHSIZE * sizeof(int *));
+  for (i = 0; i < GRAPHSIZE; i++) {
+    path[i] = INFINITY;
+  }
+  
+  return path;
+}
 
 void print_path(int *path) {
   int i;
@@ -11,7 +24,7 @@ void print_path(int *path) {
   printf("path: ");
   for (i = 0; i < GRAPHSIZE; i++) {
     if (path[i] != INFINITY)
-      printf("%i ", path[i]);
+      printf("%d ", path[i]);
   }
   printf("\n"); 
 }
@@ -21,26 +34,28 @@ void print_exec_time(int tic, int toc) {
 	 (double)(toc - tic) / CLOCKS_PER_SEC * 1000);
 }
 
-void print_cost(int *path, int **graph) {
-  int cost, i;
+void print_cost(int *path, float **graph) {
+  int i;
+  float cost;
 
   for (i = 0; i < GRAPHSIZE-1; i++) {
     if (path[i] != INFINITY && path[i+1] != INFINITY) {
       cost += graph[path[i]][path[i+1]];
     }
   }
-  printf("cost: %i\n", cost);
+  printf("cost: %.0f\n", cost);
 }
 
 int main() {
   clock_t tic, toc;
-  int **graph, *path;
+  float **graph;
+  int *path;
 
   graph = create_graph();
 
   printf("\n::dijkstra::\n");
   tic = clock();
-  path  = dijkstra_algorithm(graph);
+  path = dijkstra_algorithm(graph);
   toc = clock();
   print_path(path);
   print_cost(path, graph);
@@ -48,7 +63,15 @@ int main() {
 
   printf("::auction::\n");
   tic = clock();
-  path  = auction_algorithm(graph);
+  path = auction_algorithm(graph);
+  toc = clock();
+  print_path(path);
+  print_cost(path, graph);
+  print_exec_time(tic, toc);
+
+  printf("::auction sse::\n");
+  tic = clock();
+  path = auction_algorithm_sse(graph);
   toc = clock();
   print_path(path);
   print_cost(path, graph);
@@ -56,4 +79,3 @@ int main() {
 
   return 0;
 }
-
